fecd3732decd2958525e3852a715880f4d337135
Kyle Hantula <kylehantula@gmail.com>
2022-10-24 18:47:47 -0700

    addition of MotionZone Component

---
+++ mhr-Companion/src/app/motion_zones/motion_zones.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'pm-motion_zones',
+    templateUrl: './motion_zones.component.html'
+})
+export class Motion_ZoneComponent {
+    pageTitle: string = 'Motion Zone List'
+}
