da0a857932bf7595c7995a8933ba0b43f67154af
Kyle Hantula <kylehantula@gmail.com>
2022-11-14 11:19:52 -0600

    login feature + removed routes and input tables into front page

--- mhr-Companion/src/app/app-routing.module.ts
+++ mhr-Companion/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HitZoneComponent } from './hit-zone/hit-zone.component';
+import { LoginComponent } from './login/login.component';
 import { Motion_ZoneComponent } from './motion_zones/motion_zones.component';
 
 const routes: Routes = [
-{path: 'pm-motion_zone', component: Motion_ZoneComponent },
-{ path: 'app-hit-zone', component: HitZoneComponent }
+{ path: '', redirectTo: '/', pathMatch: 'full' },
+//{path: 'pm-motion_zone', component: Motion_ZoneComponent },
+//{ path: 'app-hit-zone', component: HitZoneComponent },
+//{ path: 'login-logout', component: LoginComponent }
 ];
 
 @NgModule({
