31dd94c9e52f1c48ccd974880a55f4ead5f774fb
Kyu Kim <kyu.kim@bellevuecollege.edu>
2022-11-15 19:02:50 -0800

    Added functionality to display motion values on the MV page. Separated pages out into their respective routes. Still working on hitzones.

---
+++ mhr-Companion/src/app/motion_zones/motion_zone.service.ts
@@ -0,0 +1,39 @@
+import {Injectable } from "@angular/core";
+import { IMotionzone } from "./motion_zone";
+import { Observable, range, map, filter, catchError, tap, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse, } from '@angular/common/http';
+
+@Injectable()
+export class MotionZoneService {
+
+
+    private hitzoneUrl = 'http://localhost:5000/mvsearch/1';
+    private hitzoneUrl2 = 'http://localhost:5000/mvsearch2/LA';
+    constructor(private http:HttpClient) { }
+
+    getOneMove(): Observable<IMotionzone[]>{
+        return this.http.get<IMotionzone[]>(this.hitzoneUrl).pipe(
+            tap(data => console.log('All: ', JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    getLanceMoves(): Observable<IMotionzone[]>{
+        return this.http.get<IMotionzone[]>(this.hitzoneUrl2).pipe(
+            tap(data => console.log('All: ', JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse){
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent){
+            errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(()=>errorMessage);
+    }
+
+}
