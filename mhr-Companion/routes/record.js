95d7182631a694d3b5b532c441da7447f53c8c74
Kyle Hantula <kylehantula@gmail.com>
2022-11-26 12:12:21 -0600

    GET, PUT, POST logic on userSettingsForm component for returning characters

--- mhr-Companion/routes/record.js
+++ mhr-Companion/routes/record.js
@@ -1,6 +1,6 @@
-const bodyParser = require('body-parser');
-const { request } = require('express');
-const express = require('express');
+const bodyParser = require("body-parser");
+const { request } = require("express");
+const express = require("express");
 
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
@@ -10,28 +10,27 @@ const recordRoutes = express.Router();
 recordRoutes.use(bodyParser.json());
 
 // This will help us connect to the database
-const dbo = require('../db/conn');
+const dbo = require("../db/conn");
 
 // This section will help you get a list of all the hitzone records.
-recordRoutes.route('/hitzones').get(async function (_req, res) {
+recordRoutes.route("/hitzones").get(async function (_req, res) {
   const dbConnect = dbo.getDb();
 
   dbConnect
-    .collection('HitzoneCollection')
+    .collection("HitzoneCollection")
     .find({})
     .limit(50)
     .toArray(function (err, result) {
       if (err) {
-        res.status(400).send('Error fetching hitzones!');
+        res.status(400).send("Error fetching hitzones!");
       } else {
         res.json(result);
       }
     });
 });
 
-
 //this route simply returns monster names and ID in order to make building the dropdown easier.
-recordRoutes.route('/hitzonenames').get(async function (_req, res) {
+recordRoutes.route("/hitzonenames").get(async function (_req, res) {
   const dbConnect = dbo.getDb();
   const hitzones = dbConnect.collection("HitzoneCollection");
   const projection = { _id: 0, MonsterName: 1, MonsterID: 1 };
@@ -39,7 +38,7 @@ recordRoutes.route('/hitzonenames').get(async function (_req, res) {
 
   cursor.toArray(function (err, result) {
     if (err) {
-      res.status(400).send('Error fetching hitzones!');
+      res.status(400).send("Error fetching hitzones!");
     } else {
       res.json(result);
     }
@@ -47,24 +46,23 @@ recordRoutes.route('/hitzonenames').get(async function (_req, res) {
 });
 
 //this route returns all motion values
-recordRoutes.route('/motionvalues').get(async function (_req, res) {
+recordRoutes.route("/motionvalues").get(async function (_req, res) {
   const dbConnect = dbo.getDb();
 
   dbConnect
-    .collection('WeaponCollection')
+    .collection("WeaponCollection")
     .find({})
     .limit(50)
     .toArray(function (err, result) {
       if (err) {
-        res.status(400).send('Error fetching MVs!');
+        res.status(400).send("Error fetching MVs!");
       } else {
         res.json(result);
       }
     });
 });
 
-
-recordRoutes.route('/hzsearch/:id').get(async function (_req, res) {
+recordRoutes.route("/hzsearch/:id").get(async function (_req, res) {
   console.log(_req.params.id);
   const requestID = parseInt(_req.params.id);
   const dbConnect = dbo.getDb();
@@ -76,14 +74,14 @@ recordRoutes.route('/hzsearch/:id').get(async function (_req, res) {
 
   cursor.toArray(function (err, result) {
     if (err) {
-      res.status(400).send('Error fetching hitzones!');
+      res.status(400).send("Error fetching hitzones!");
     } else {
       res.json(result);
     }
   });
 });
 
-recordRoutes.route('/mvsearch/:id').get(async function (_req, res) {
+recordRoutes.route("/mvsearch/:id").get(async function (_req, res) {
   console.log(_req.params.id);
   const requestID = parseInt(_req.params.id);
   const dbConnect = dbo.getDb();
@@ -95,15 +93,14 @@ recordRoutes.route('/mvsearch/:id').get(async function (_req, res) {
 
   cursor.toArray(function (err, result) {
     if (err) {
-      res.status(400).send('Error fetching weapons!');
+      res.status(400).send("Error fetching weapons!");
     } else {
       res.json(result);
     }
   });
 });
 
-
-recordRoutes.route('/mvsearch2/:wep').get(async function (_req, res) {
+recordRoutes.route("/mvsearch2/:wep").get(async function (_req, res) {
   console.log(_req.params.wep);
   const requestID = _req.params.wep.toString();
   const dbConnect = dbo.getDb();
@@ -115,14 +112,14 @@ recordRoutes.route('/mvsearch2/:wep').get(async function (_req, res) {
 
   cursor.toArray(function (err, result) {
     if (err) {
-      res.status(400).send('Error fetching weapons!');
+      res.status(400).send("Error fetching weapons!");
     } else {
       res.json(result);
     }
   });
 });
 
-recordRoutes.route('/userSettings/:userid').get(async function (_req, res) {
+recordRoutes.route("/userSettings/:userid").get(async function (_req, res) {
   const dbConnect = dbo.getDb();
 
   console.log(_req.params.userid);
@@ -135,139 +132,112 @@ recordRoutes.route('/userSettings/:userid').get(async function (_req, res) {
 
   cursor.toArray(function (err, result) {
     if (err) {
-      res.status(400).send('Error fetching userinfo!');
+      res.status(400).send("Error fetching userinfo!");
     } else {
       res.json(result);
     }
   });
 });
 
-recordRoutes.route('/userSettings').get(async function (_req, res) {
+recordRoutes.route("/userSettings").get(async function (_req, res) {
   const dbConnect = dbo.getDb();
 
   dbConnect
-    .collection('UserInputCollection')
+    .collection("UserInputCollection")
     .find({})
     .limit(50)
     .toArray(function (err, result) {
       if (err) {
-        res.status(400).send('Error fetching MVs!');
+        res.status(400).send("Error fetching MVs!");
       } else {
         res.json(result);
       }
     });
 });
 
-
 // This section will help you create a new record.
-recordRoutes.route('/userSettings/recordUserSettings').post(function (req, res) {
-  const dbConnect = dbo.getDb();
-  
-  const userSettings = {
-    googleid: req.body.googleid,
-    last_modified: new Date(),
-    raw: req.body.raw,
-    sharpness: req.body.sharpness,
-    eleType: req.body.eleType,
-    ele: req.body.ele,
-    critchance: req.body.critchance,
-    wex: req.body.wex,
-    critboost: req.body.critboost,
-    criteye: req.body.criteye,
-    atkboost: req.body.atkboost,
-    agitator: req.body.agitator,
-    peakperf: req.body.peakperf,
-    resentment: req.body.resentment,
-    resuscitate: req.body.resuscitate,
-    maxmight: req.body.maxmight,
-    critele: req.body.critele,
-    offensiveguard: req.body.offensiveguard,
-    eleatkup: req.body.eleatkup,
-    eleexploit: req.body.eleexploit,
-    mailofhellfire: req.body.mailofhellfire,
-    dereliction: req.body.dereliction,
-    burst: req.body.burst 
-  };
+recordRoutes
+  .route("/userSettings/recordUserSettings")
+  .post(function (req, res) {
+    const dbConnect = dbo.getDb();
 
-  dbConnect
-    .collection('UserInputCollection')
-    .insertOne(userSettings, function (err, result) {
-      if (err) {
-        res.status(400).send('Error inserting matches!');
-      } else {
-        console.log(`Added a new match with id ${result.insertedId}`);
-        res.status(204).send();
-      }
-    });
-});
+    const userSettings = {
+      googleid: req.body.googleid,
+      last_modified: new Date(),
+      raw: req.body.raw,
+      sharpness: req.body.sharpness,
+      eleType: req.body.eleType,
+      ele: req.body.ele,
+      critchance: req.body.critchance,
+      wex: req.body.wex,
+      critboost: req.body.critboost,
+      criteye: req.body.criteye,
+      atkboost: req.body.atkboost,
+      agitator: req.body.agitator,
+      peakperf: req.body.peakperf,
+      resentment: req.body.resentment,
+      resuscitate: req.body.resuscitate,
+      maxmight: req.body.maxmight,
+      critele: req.body.critele,
+      offensiveguard: req.body.offensiveguard,
+      eleatkup: req.body.eleatkup,
+      eleexploit: req.body.eleexploit,
+      mailofhellfire: req.body.mailofhellfire,
+      dereliction: req.body.dereliction,
+      burst: req.body.burst,
+    };
 
-recordRoutes.route('/userSettings/overwriteUserSettings').put(function (req, res) {
-  const dbConnect = dbo.getDb();
-  const requestID = parseInt(req.params.googleid);
-  const query = { googleid: requestID };
-  const cursor = userinfo.find(query);
-  const userSettings = {
-    googleid: req.body.googleid,
-    last_modified: new Date(),
-    raw: req.body.raw,
-    sharpness: req.body.sharpness,
-    eleType: req.body.eleType,
-    ele: req.body.ele,
-    critchance: req.body.critchance,
-    wex: req.body.wex,
-    critboost: req.body.critboost,
-    criteye: req.body.criteye,
-    atkboost: req.body.atkboost,
-    agitator: req.body.agitator,
-    peakperf: req.body.peakperf,
-    resentment: req.body.resentment,
-    resuscitate: req.body.resuscitate,
-    maxmight: req.body.maxmight,
-    critele: req.body.critele,
-    offensiveguard: req.body.offensiveguard,
-    eleatkup: req.body.eleatkup,
-    eleexploit: req.body.eleexploit,
-    mailofhellfire: req.body.mailofhellfire,
-    dereliction: req.body.dereliction,
-    burst: req.body.burst 
-  };
-  dbConnect
-    .collection('UserInputCollection')
-    cursor.updateOne(userSettings, function (err, result) {
-      if (err) {
-        res.status(400).send('Error updating stats!');
-      } else {
-        console.log(`Updated stats with id ${req.insertedId}`);
-        res.status(204).send();
-      }
-    });
-});
+    dbConnect
+      .collection("UserInputCollection")
+      .insertOne(userSettings, function (err, result) {
+        if (err) {
+          res.status(400).send("Error inserting matches!");
+        } else {
+          console.log(`Added a new match with id ${result.insertedId}`);
+          res.status(204).send();
+        }
+      });
+  });
 
+recordRoutes
+  .route("/userSettings/overwriteUserSettings")
+  .post(async function (req, res) {
+    const dbConnect = dbo.getDb();
+    const userinfo = dbConnect.collection("UserInputCollection");
+    const idcheck = req.body.googleid;
 
-
-
-// // This section will help you update a record by id.
-// recordRoutes.route('/listings/updateLike').post(function (req, res) {
-//   const dbConnect = dbo.getDb();
-//   const listingQuery = { _id: req.body.id };
-//   const updates = {
-//     $inc: {
-//       likes: 1,
-//     },
-//   };
-
-//   dbConnect
-//     .collection('listingsAndReviews')
-//     .updateOne(listingQuery, updates, function (err, _result) {
-//       if (err) {
-//         res
-//           .status(400)
-//           .send(`Error updating likes on listing with id ${listingQuery.id}!`);
-//       } else {
-//         console.log('1 document updated');
-//       }
-//     });
-// });
+    const userSettings = req.body;
+    console.log(userSettings);
+    userinfo.replaceOne(
+      { googleid: idcheck },
+      {
+        last_modified: new Date(),
+        raw: req.body.raw,
+        sharpness: req.body.sharpness,
+        eleType: req.body.eleType,
+        ele: req.body.ele,
+        critchance: req.body.critchance,
+        wex: req.body.wex,
+        critboost: req.body.critboost,
+        criteye: req.body.criteye,
+        atkboost: req.body.atkboost,
+        agitator: req.body.agitator,
+        peakperf: req.body.peakperf,
+        resentment: req.body.resentment,
+        resuscitate: req.body.resuscitate,
+        maxmight: req.body.maxmight,
+        critele: req.body.critele,
+        offensiveguard: req.body.offensiveguard,
+        eleatkup: req.body.eleatkup,
+        eleexploit: req.body.eleexploit,
+        mailofhellfire: req.body.mailofhellfire,
+        dereliction: req.body.dereliction,
+        burst: req.body.burst,
+      },
+      { upsert: true }
+    );
+    console.log(`updated ${req.body.googleid} character stats`);
+  });
 
 // // This section will help you delete a record.
 // recordRoutes.route('/listings/delete/:id').delete((req, res) => {
